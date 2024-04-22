6363a22f35fcce0d73522c262255d12369463704
maja rotovnik <maja.rotovnik001@gmail.com>
2024-04-22 16:35:45 +0200

    fixed health service

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const axios = require('axios');
 const app = express();
 const PORT = 1000;
 
-// Definiramo URL-je za health check
 const healthCheckUrls = [
     { url: 'http://127.0.0.1:8082/q/health', name: 'Payment Service' },
     { url: 'http://localhost:8081/actuator/health', name: 'Users Service' }
 ];
 
-// Izvedemo health check za vsak URL
 async function performHealthCheck() {
     const healthChecks = [];
 
@@ -26,7 +24,6 @@ async function performHealthCheck() {
     return healthChecks;
 }
 
-// Endpoint za health check
 app.get('/health', async (req, res) => {
     try {
         const healthStatus = await performHealthCheck();
@@ -36,7 +33,6 @@ app.get('/health', async (req, res) => {
     }
 });
 
-// Poslušamo na določenem portu
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
